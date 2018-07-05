73a48ed4811323b6d5e27be720f91df5f9e85d80
Annie Shin, Rita Bennett-Chew and Will Swanson <rita@>
2018-07-05 11:30:49 -0700

    display correctly in local host web browser

--- app.rb
+++ app.rb
@@ -2,3 +2,17 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/tamagotchi')
+require('pry')
+
+
+post('/') do
+  @my_pet = Tamagotchi.new({:name => "lil dragon"})
+  erb(:input)
+end
+
+# post('/') do
+#   name = params["name"]
+#   item = Item.new(name)
+#   item.save()
+#   erb(:)
+# end
